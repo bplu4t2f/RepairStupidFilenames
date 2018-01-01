fecc03ba25732546ad76db33700ecb26b8193c84
bplu4t2f <bplu4t2f@gmail.com>
2018-01-01 20:44:00 +0100

    initial commit

---
+++ RepairStupidFilenames/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace RepairStupidFilenames
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonAnalyze = new System.Windows.Forms.Button();
+			this.labelFolderHint = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.buttonAccept = new System.Windows.Forms.Button();
+			this.listViewPreview = new System.Windows.Forms.ListView();
+			this.columnHeaderOld = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.columnHeaderNew = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+			this.textBoxPrefix = new System.Windows.Forms.TextBox();
+			this.labelPrefixHint = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// buttonAnalyze
+			// 
+			this.buttonAnalyze.Location = new System.Drawing.Point(55, 96);
+			this.buttonAnalyze.Name = "buttonAnalyze";
+			this.buttonAnalyze.Size = new System.Drawing.Size(75, 23);
+			this.buttonAnalyze.TabIndex = 0;
+			this.buttonAnalyze.Text = "Analyze";
+			this.buttonAnalyze.UseVisualStyleBackColor = true;
+			this.buttonAnalyze.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// labelFolderHint
+			// 
+			this.labelFolderHint.AutoSize = true;
+			this.labelFolderHint.Location = new System.Drawing.Point(12, 28);
+			this.labelFolderHint.Name = "labelFolderHint";
+			this.labelFolderHint.Size = new System.Drawing.Size(64, 13);
+			this.labelFolderHint.TabIndex = 1;
+			this.labelFolderHint.Text = "File or folder";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(82, 25);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(190, 20);
+			this.textBox1.TabIndex = 2;
+			// 
+			// buttonAccept
+			// 
+			this.buttonAccept.Location = new System.Drawing.Point(183, 161);
+			this.buttonAccept.Name = "buttonAccept";
+			this.buttonAccept.Size = new System.Drawing.Size(75, 23);
+			this.buttonAccept.TabIndex = 4;
+			this.buttonAccept.Text = "Accept";
+			this.buttonAccept.UseVisualStyleBackColor = true;
+			this.buttonAccept.Click += new System.EventHandler(this.buttonAccept_Click);
+			// 
+			// listViewPreview
+			// 
+			this.listViewPreview.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.listViewPreview.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderOld,
+            this.columnHeaderNew});
+			this.listViewPreview.FullRowSelect = true;
+			this.listViewPreview.GridLines = true;
+			this.listViewPreview.HideSelection = false;
+			this.listViewPreview.Location = new System.Drawing.Point(295, 12);
+			this.listViewPreview.Name = "listViewPreview";
+			this.listViewPreview.Size = new System.Drawing.Size(677, 485);
+			this.listViewPreview.TabIndex = 5;
+			this.listViewPreview.UseCompatibleStateImageBehavior = false;
+			this.listViewPreview.View = System.Windows.Forms.View.Details;
+			// 
+			// columnHeaderOld
+			// 
+			this.columnHeaderOld.Text = "Old";
+			this.columnHeaderOld.Width = 298;
+			// 
+			// columnHeaderNew
+			// 
+			this.columnHeaderNew.Text = "New";
+			this.columnHeaderNew.Width = 312;
+			// 
+			// textBoxPrefix
+			// 
+			this.textBoxPrefix.Location = new System.Drawing.Point(97, 52);
+			this.textBoxPrefix.Name = "textBoxPrefix";
+			this.textBoxPrefix.Size = new System.Drawing.Size(175, 20);
+			this.textBoxPrefix.TabIndex = 6;
+			// 
+			// labelPrefixHint
+			// 
+			this.labelPrefixHint.AutoSize = true;
+			this.labelPrefixHint.Location = new System.Drawing.Point(12, 55);
+			this.labelPrefixHint.Name = "labelPrefixHint";
+			this.labelPrefixHint.Size = new System.Drawing.Size(79, 13);
+			this.labelPrefixHint.TabIndex = 1;
+			this.labelPrefixHint.Text = "Prefix (optional)";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(984, 509);
+			this.Controls.Add(this.textBoxPrefix);
+			this.Controls.Add(this.listViewPreview);
+			this.Controls.Add(this.buttonAccept);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.labelPrefixHint);
+			this.Controls.Add(this.labelFolderHint);
+			this.Controls.Add(this.buttonAnalyze);
+			this.Name = "Form1";
+			this.Text = "Repair stupid file names";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonAnalyze;
+		private System.Windows.Forms.Label labelFolderHint;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button buttonAccept;
+		private System.Windows.Forms.ListView listViewPreview;
+		private System.Windows.Forms.ColumnHeader columnHeaderOld;
+		private System.Windows.Forms.ColumnHeader columnHeaderNew;
+		private System.Windows.Forms.TextBox textBoxPrefix;
+		private System.Windows.Forms.Label labelPrefixHint;
+	}
+}
+
---
+++ RepairStupidFilenames/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RepairStupidFilenames
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private List<(string oldName, string newName)> pendingActions;
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var file = this.textBox1.Text;
+			if (String.IsNullOrWhiteSpace(file))
+			{
+				MessageBox.Show("user pls");
+				return;
+			}
+			var prefix = this.textBoxPrefix.Text;
+
+			var errors = new List<(string file, Exception ex)>();
+			var infoList = new List<Info>();
+
+			if (Directory.Exists(file))
+			{
+				foreach (var child in Directory.GetFiles(file))
+				{
+					this.ProcessFile(prefix, Path.Combine(file, child), errors, infoList);
+				}
+			}
+			else
+			{
+				this.ProcessFile(prefix, file, errors, infoList);
+			}
+
+			if (errors.Any())
+			{
+				var message = "Failed to analyze some files. Continue anyway?" + Environment.NewLine + String.Join(Environment.NewLine, errors.Select(x => $"{x.file}: {x.ex.Message}"));
+				var choice = MessageBox.Show(this, message, "Analyze error", MessageBoxButtons.YesNo);
+				if (choice != DialogResult.Yes)
+				{
+					return;
+				}
+			}
+
+			if (!infoList.Any())
+			{
+				return;
+			}
+
+			int trackNumberLength;
+			{
+				var maxTrackNumber = infoList.Max(x => x.trackNumber);
+				// At least 2, because single digit track numbers are sad
+				trackNumberLength = Math.Min(maxTrackNumber.ToString().Length, 2);
+			}
+
+			this.pendingActions = new List<(string oldName, string newName)>();
+			{
+				var invalidFileNameChars = Path.GetInvalidFileNameChars();
+				foreach (var info in infoList)
+				{
+					string title = info.title;
+					foreach (var c in invalidFileNameChars)
+					{
+						title = title.Replace(c, '_');
+					}
+
+					string extension = Path.GetExtension(info.originalFilePath);
+					string trackNumber = info.trackNumber.ToString("D" + trackNumberLength);
+
+					var newFileName = $"{prefix}{trackNumber} {title}{extension}";
+					var newFilePath = Path.Combine(Path.GetDirectoryName(info.originalFilePath), newFileName);
+					this.pendingActions.Add((info.originalFilePath, newFilePath));
+				}
+			}
+
+			this.listViewPreview.Items.Clear();
+			foreach (var action in this.pendingActions)
+			{
+				Debug.WriteLine("OLD: " + action.oldName);
+				Debug.WriteLine("NEW: " + action.newName);
+				var item = this.listViewPreview.Items.Add(Path.GetFileName(action.oldName));
+				item.SubItems.Add(Path.GetFileName(action.newName));
+			}
+		}
+
+		class Info
+		{
+			public Info(string originalFilePath, string title, uint trackNumber)
+			{
+				this.originalFilePath = originalFilePath;
+				this.title = title;
+				this.trackNumber = trackNumber;
+			}
+
+			public string originalFilePath;
+			public string title;
+			public uint trackNumber;
+		}
+
+		private void ProcessFile(string prefix, string filePath, List<(string, Exception)> errors, List<Info> infoList)
+		{
+			try
+			{
+				string title;
+				uint trackNumber;
+
+				using (var tagFile = TagLib.File.Create(filePath))
+				{
+					title = tagFile.Tag.Title;
+					trackNumber = tagFile.Tag.Track;
+					if (String.IsNullOrWhiteSpace(title) || trackNumber == 0)
+					{
+						return;
+					}
+				}
+
+				infoList.Add(new Info(filePath, title, trackNumber));
+			}
+			catch (Exception ex)
+			{
+				errors.Add((filePath, ex));
+				return;
+			}
+
+		}
+
+		private void buttonAccept_Click(object sender, EventArgs e)
+		{
+			if (this.pendingActions == null)
+			{
+				return;
+			}
+
+			try
+			{
+				foreach (var file in this.pendingActions)
+				{
+					File.Move(file.oldName, file.newName);
+				}
+
+				this.pendingActions = null;
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("Error moving files." + Environment.NewLine + ex.ToString());
+			}
+		}
+	}
+}
